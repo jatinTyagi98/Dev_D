c734e71c2e660a2a0bf30f04fbf797c095f7f5b4
jatinTyagi98 <jatintyagi97@gmail.com>
2022-04-08 22:19:23 +0530

    cells are created

--- module_2/Excel_clone/script.js
+++ module_2/Excel_clone/script.js
@@ -19,7 +19,7 @@ for(let j=0;j<26;j++){
     //for eg: 65 + 1 = 66
     let ascii = 65 + j;
     //js function is used for to convert ascii code to alphabet
-    //for 66 , aplhabet will be B
+    //for 66 , alphabet will be B
     let requireAlphabet = String.fromCharCode(ascii);
     //creating div everytime till loop works
     let div = document.createElement("div");
@@ -30,4 +30,42 @@ for(let j=0;j<26;j++){
 
     //appending in the parent div
     columnNumberSection.append(div);
+}
+
+let cellSectionDiv = document.querySelector(".cell-section");
+
+//WE have to make a matrix of [100 x 26] 
+//[A1,B1 ... Z1]
+//.
+//.
+//[A100,B100 ... Z100] -> in the form of cells
+//this loop make the 100 rows
+for(let i=1;i<=100;i++){
+    let rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+    //this loop make 26(A to Z) cells
+    for(let j=0;j<26;j++){
+
+        let ascii = 65 + j;
+
+        let requireAlphabet = String.fromCharCode(ascii);
+        //we will store a cell address 
+        //requireAlphabet = A , i = 1 
+        //cellAddress -> A1
+        let cellAddress = requireAlphabet + i;
+
+        //this will be the div for cells
+        let cellDiv = document.createElement("div");
+        cellDiv.classList.add("cell");
+
+        //setting an attribute as cellAddress
+        cellDiv.setAttribute("cell-address", cellAddress);
+
+        //appending all 26 cells in row
+        rowDiv.append(cellDiv);
+
+
+    }
+    //appending them in the cell-section
+    cellSectionDiv.append(rowDiv);
 }
